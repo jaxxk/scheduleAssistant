e8e9b50489febe00de2a88b23ebc42437f1d6b2d
jaxxk <forwarwick@live.com>
2021-09-19 17:48:57 -0700

    added sample tasks

--- schedule-assistant/src/App.js
+++ schedule-assistant/src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+
 import React from 'react'
+import {useState} from 'react'
 
 const App = () => {
   
+  const [tasks,setTasks] = useState( [
+    {
+        id: 1,
+        text: "appointment",
+        time: "3 hours",
+        reminder:true,
+    },
+    {
+        id: 2,
+        text: "doc appointment",
+        time: "3 hours",
+        reminder:false,
+    },
+  ]);
+
   return (
     <div className="container">
       <Header/>
+      <Tasks tasks={tasks}/>
     </div>
   )
 }
--- schedule-assistant/src/components/Task.js
+++ schedule-assistant/src/components/Task.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { FaTimes } from 'react-icons/fa'
 
-const Task = () => {
-   return (
-      <div>
-         
-      </div>
-   )
+const Task = ({task}) => {
+    return (
+        <div className='task'>
+            <h3>
+                {task.text}
+                <FaTimes style={{color:
+                'red', cursor:'pointer'}} onClick={ () => console.log('ran')}/> 
+                {/* //state gets passed down and "actions gets passed up */}
+            </h3>
+            <p>{task.time}</p>
+
+        </div>
+    )
 }
 
 export default Task
