b8a6f33d1ecc9202ddeaf6670e30afcfe29ca0ad
jaxxk <jackwei2018@outlook.com>
2021-09-22 21:32:27 -0700

    finished the timer

--- schedule-assistant/src/App.js
+++ schedule-assistant/src/App.js
@@ -1,24 +1,23 @@
 import './App.css';
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-
 import React from 'react'
 import {useState} from 'react'
 
 const App = () => {
   
-  const [showTimer, setShowTimer] = useState(false)
+
   const [tasks,setTasks] = useState( [
     {
         id: 1,
         text: "appointment",
-        time: "3 hours",
+        time: "0:0:5",
         start:false,
     },
     {
         id: 2,
         text: "doc appointment",
-        time: "3 hours",
+        time: "0:4:10",
         start:false,
     },
   ]);
@@ -31,14 +30,13 @@ const startTask = (id) => {
   setTasks(tasks.map((task) => (
     task.id === id ? {...task, start:!task.start} : task
   )))
-
+  
 }
 
   return (
     <div className="container">
       <Header/>
       <Tasks tasks={tasks} onDelete={onDelete} startTask={startTask}/>
-      {showTimer ? <Countdown/> : ''}
     </div>
   )
 }
--- schedule-assistant/src/components/Task.js
+++ schedule-assistant/src/components/Task.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
+import Countdown from 'react-countdown';
+
 
 const Task = ({task,onDelete,startTask}) => {
     
     const startTimer = () => {
         startTask(task.id)
+    }
+
+    // Renderer callback with condition
+    const renderer = ({ hours, minutes, seconds, completed }) => {
+        if (completed) {
+            // Render a completed state
+            return null
+        } else {
+            // Render a countdown
+            return <span>{hours}:{minutes}:{seconds}</span>;
+        }
+    };
+
+    const convertTime = (time) => {
+        let hour = parseInt(time.slice(0,2))*60*60*1000;
+        let mins = parseInt(time.slice(2,4))*60*1000;
+        let seconds = parseInt(time.slice(4,6))*1000;
+        time = hour + mins+seconds;
+        return time;
     }
     
     return (
@@ -15,7 +36,11 @@ const Task = ({task,onDelete,startTask}) => {
                 'red', cursor:'pointer'}} onClick={() => onDelete(task.id)}/> 
                 {/* //state gets passed down and "actions gets passed up */}
             </h3>
-            <p>{task.time}</p>
+            {/* <p>{task.time}</p> */}
+            {task.start ? <Countdown
+            date={Date.now() + convertTime(task.time)}
+            renderer={renderer}
+            onComplete={() => onDelete(task.id)}/> : <p>Scheduled time: {task.time}</p>}
 
         </div>
     )
